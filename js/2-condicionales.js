5b7af1e213a90a16ce06637473d17587ad7f205b
Anto Ortega <antonellaportega@gmail.com>
2023-10-23 20:19:19 -0300

    vimos condicional simple

---
+++ js/2-condicionales.js
@@ -0,0 +1,10 @@
+// if (condicion logica)
+
+//Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres
+
+let num1 = parseInt(prompt("ingrese el primer numero"));
+let num2 = parseInt(prompt("ingrese el segundo numero"));
+
+if (num1>num2){
+    console.log("la variable numero 1 es mayor que la variable numero 2");
+}
