451fbed7f1cca8a0a0ae59148b3b3747ae0c0c2b
Anto Ortega <antonellaportega@gmail.com>
2023-10-23 20:41:30 -0300

    ingreso dos numeros y los sumo

---
+++ js/Ejercicio_5.js
@@ -0,0 +1,6 @@
+// Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números
+
+let num1 = parseInt(prompt("Ingrese el primer num ero"));
+let num2 = parseInt(prompt("ingrese el segundo numero"));
+
+document.write(num1+num2);
