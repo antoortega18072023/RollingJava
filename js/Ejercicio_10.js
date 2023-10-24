b418c9a9cfc55d8967bf83864ce61fc67bd838f6
Anto Ortega <antonellaportega@gmail.com>
2023-10-24 16:20:27 -0300

    numero si es divisible por 2,3,5,7 o 10

---
+++ js/Ejercicio_10.js
@@ -0,0 +1,16 @@
+
+// Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+let numero = parseInt(prompt("Introduce un número:"));
+
+// Verificar si es divisible por 2, 3, 5 o 7
+if (numero % 2 === 0) {
+    console.log("El " + numero + " es divisible por 2.");
+} else if (numero % 3 === 0) {
+    console.log("El " + numero + " es divisible por 3.");
+} else if (numero % 5 === 0) {
+    console.log("El " + numero + " es divisible por 5.");
+} else if (numero % 7 === 0) {
+    console.log("El " + numero + " es divisible por 7.");
+} else {
+    console.log("El " + numero + " no es divisible por 2, 3, 5 ni 7.");
+}
