52ad8e8e13516b4eafa34660fb7849c03736b2ef
Anto Ortega <antonellaportega@gmail.com>
2023-10-23 21:02:18 -0300

    verificamos is un numero es divisible por dos

---
+++ js/Ejercicio_8.js
@@ -0,0 +1,9 @@
+// Escribe un programa que pida un número y diga si es divisible por 2
+
+let num = parseInt(prompt("ingrese un numero"));
+
+if (num % 2 === 0){
+    console.log(num + " es divisible por 2");
+} else{
+    console.log(num + " NO es divisible por 2");
+}
